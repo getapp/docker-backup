a40b28b4f259dc92d2d41f0660d6f442511d488c
Boris Mikhaylov <kaguxmail@gmail.com>
2016-05-04 10:42:18 +0200

    use single var to set backup interval

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Add `backup` gem models to `models` dir. Model names must match filenames.
 
-To configure backup interval use `BACKUP_INTERVAL_VALUE` and `BACKUP_INTERVAL_UNIT` environment variables.
+To configure backup interval use `BACKUP_INTERVAL` environment variable.
--- schedule.rb
+++ schedule.rb
@@ -4,10 +4,6 @@ models = Dir[File.join __dir__, 'models', '*.rb']
          .map { |p| File.basename(p, '.rb') }
          .map(&:to_sym)
 
-interval_value = (ENV['BACKUP_INTERVAL_VALUE'] || 1).to_i
-interval_unit = ENV['BACKUP_INTERVAL_UNIT'] || 'day'
-interval = interval_value.send(interval_unit.to_sym)
-
-every interval do
+every ENV['BACKUP_INTERVAL'] do
   models.each { backup model }
 end
